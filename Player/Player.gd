b99056f765adcdb60d302f9fa06884b79914541e
Nhat180 <s3740975@rmit.edu.vn>
2021-10-13 22:59:41 +0700

    Update Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,7 @@ export var SPEED = 120
 export var ACCEL = 450
 export var FRICTION = 450
 
-
+#Types of action of player
 enum {
 	MOVE,
 	ATTACK
@@ -26,6 +26,7 @@ func _ready():
 	animationTree.active = true
 	hitboxCollision.disabled = true
 
+#Handle the physic process of player
 func _physics_process(delta):
 	match state:
 		MOVE:
@@ -36,6 +37,7 @@ func _physics_process(delta):
 	
 	
 
+# Move state, handle the sprite movement
 func moveState(delta):
 	var vector = Vector2.ZERO
 	vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -52,23 +54,26 @@ func moveState(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
+	#Ensure the player stays in the playing screen
 	position.x = clamp(position.x, 0, screenSize.x)
 	position.y = clamp(position.y, 0, screenSize.y - 80)
-	velocity = move_and_slide(velocity)
+	
+	velocity = move_and_slide(velocity) 
 	
 	if item.sword >= 1:
 		hitboxCollision.disabled = false
 	else:
 		hitboxCollision.disabled = true
 
-
+# Attack state, handle sprite attack
 func attackState():
 	velocity = move_and_slide(Vector2.ZERO)
 	animationState.travel("Attack")
 
+# Ensure player will change from attack to move state after finishing the attack animation sprite
 func attack_animation_finished():
 	state = MOVE
 
-
+# Detect the enemy in attack range and give order to player to change from move to attack state 
 func _on_Hitbox_area_entered(area):
 	state = ATTACK
