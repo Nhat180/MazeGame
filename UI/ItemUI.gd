7edf4bfd53fd6a6f22f37304c8e6f5bd79f40cf6
Nhat180 <s3740975@rmit.edu.vn>
2021-10-14 23:02:36 +0700

    Update ItemUI.gd

--- UI/ItemUI.gd
+++ UI/ItemUI.gd
@@ -8,14 +8,17 @@ var coin setget setCoinUI
 var sword setget setSwordUI 
 var key setget setKeyUI 
 
+# Update number of coin on game UI
 func setCoinUI(value):
 	coin = clamp(value, 0, 8)
 	coinUI.rect_size.x = coin * 16
 
+# Update number sword on game UI
 func setSwordUI(value):
 	sword = clamp(value, 0, 8)
 	swordUI.rect_size.x = sword * 384
 
+# Update number key on game UI
 func setKeyUI(value):
 	key = value
 	keyUI.rect_size.x = key * 16
