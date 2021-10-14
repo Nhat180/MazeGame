4d5ec884edae2ddde54085f548263295adf67626
Nhat180 <s3740975@rmit.edu.vn>
2021-10-14 22:57:33 +0700

    Update ItemCollect.gd

--- Item/ItemCollect.gd
+++ Item/ItemCollect.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var itemStat = ItemStats
 
-
+# Update the coins, swords and key stat when player collect the correponding item
 func _on_Coin_body_entered(body):
 	itemStat.coin += 1
 	queue_free()
